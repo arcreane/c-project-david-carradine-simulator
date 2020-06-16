19abec62dafde3e382a6b8fd6023572da0d3a8d8
elie <edarouj@hotmail.com>
2020-06-16 16:16:43 +0200

    test

--- pendu/Program.cs
+++ pendu/Program.cs
@@ -12,6 +12,7 @@ namespace pendu
         {
             Console.WriteLine("yann est passe par la");
             Console.WriteLine("Ariel t'attendais");
+             Console.WriteLine("elie t'attendais");
         }
     }
 }
